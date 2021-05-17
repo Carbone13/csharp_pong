08e1628cdd3eaf5aa7aea6d31ee952ebbc150956
Lucas Michaudel <38987429+Carbone13@users.noreply.github.com>
2021-05-18 01:20:57 +0200

    Initial Commit

---
+++ csharp_pong/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp_pong
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main ()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
